cd1d9d10a66abf31ec4675547cf1cef6e1cc5b66
Suleyman hp <abdsul3434@gmail.com>
2021-02-13 20:03:57 +0300

    Upper-Bounded Wildcards. method that calculates sum of list of any type.

--- src/main/java/chapter1/ListWorker.java
+++ src/main/java/chapter1/ListWorker.java
@@ -3,10 +3,18 @@ package chapter1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListPrinter {
+public class ListWorker {
     public static void printList(List<?> list){
         for(Object obj: list)
             System.out.println(obj);
+    }
+
+    //calculates sum of list
+    public static long sumList(List<? extends Number> list){
+        long sum = 0;
+        for(Number number: list)
+            sum += number.longValue();
+        return sum;
     }
 
     public static void main(String[] args) {
---
+++ src/test/java/chapter1/ListWorkerTest.java
@@ -0,0 +1,55 @@
+package chapter1;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ListWorkerTest {
+
+    @Test
+    void printListStrings() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("aaaaaa");
+        stringList.add("bbbbbbb");
+        stringList.add("ccccccc");
+        ListWorker.printList(stringList);
+    }
+    @Test
+    void printListInts() {
+        List<Integer> integerListList = new ArrayList<>();
+        integerListList.add(33);
+        integerListList.add(22);
+        integerListList.add(11);
+        ListWorker.printList(integerListList);
+    }
+
+    @Test
+    void testSumListWithIntegerList() {
+        List<Integer> integerList = Arrays.asList(1, 1, 1, 1, 1);
+       long result = ListWorker.sumList(integerList);
+       assertEquals(5, result);
+    }
+
+    @Test
+    void testSumListWithLongList() {
+        int numberOfOnes = 10;
+        List<Long> integerList = Stream.generate(() -> 1L).limit(numberOfOnes).collect(Collectors.toList());
+       long result = ListWorker.sumList(integerList);
+       assertEquals(numberOfOnes, result);
+    }
+
+    @Test
+    void testSumListWithDoubleList() {
+        long numberOfOnes = 10;
+        List<Double> integerList = Stream.generate(() -> 1.0).limit(numberOfOnes).collect(Collectors.toList());
+       long result = ListWorker.sumList(integerList);
+       assertEquals(numberOfOnes, result);
+    }
+}
